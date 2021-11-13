0d7be6f93082c03228393d57737a7e87b759b60b
Akinori <aki10.working@gmail.com>
2021-11-13 23:59:10 +0900

    add delete user button

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -76,6 +76,8 @@ class UserController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $user = User::find($id);
+        $user->delete();
+        return $user;
     }
 }
